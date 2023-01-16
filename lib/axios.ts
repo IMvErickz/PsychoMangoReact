022fe354fc02410ad021bccf54ba8198ffc36929
IMvErickz <erickspy2003@gmail.com>
2023-01-16 11:35:33 -0300

    test: modified host axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import  axios  from "axios";
 
 export const api = axios.create({
-    baseURL: "http://us-east.connect.psdb.cloud:3838"
+    baseURL: "http://us-east.connect.psdb.cloud:3306"
 })
 //us-east.connect.psdb.cloud
