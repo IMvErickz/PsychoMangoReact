2cf9b7278e89d4f61f6230ebb36c470e96a287ec
IMvErickz <erickspy2003@gmail.com>
2023-01-20 10:52:42 -0300

    modified server

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import  axios  from "axios";
 
 export const api = axios.create({
-    baseURL: "https://psychoserver-imverickz.vercel.app"
+    baseURL: "https://psychoserver-csn8ivu8s-imverickz.vercel.app"
 })
 //us-east.connect.psdb.cloud
