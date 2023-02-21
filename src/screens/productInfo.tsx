59774b97d3a36ffab4f100522e957e50dd50bbb2
IMvErickz <erickspy2003@gmail.com>
2023-02-21 12:21:19 -0300

    modified the loyalt for products info

--- src/screens/productInfo.tsx
+++ src/screens/productInfo.tsx
@@ -36,16 +36,21 @@ export function Info() {
                <li key={infos.img}>
                          <div className='w-full flex flex-row items-center justify-center gap-x-96'>
                 <div className='flex flex-col items-center justify-center'>
-                    <img src={infos.img} alt="" className='w-60' />
+                    <fieldset className='w-64 bg-grayBackInput rounded-lg flex flex-col items-center justify-center'>
+                        <img src={infos.img} alt="" className='w-60' />
+                    </fieldset>
                 </div>
                 <div className='flex flex-col items-center justify-center gap-y-28'>
-                    <div className='flex flex-col items-center justify-center bg-grayBackInput p-3 rounded-3xl'>
+                    <div className='flex flex-col items-center justify-center p-3 rounded-3xl'>
                         <h1 className='text-white text-6xl'>{infos.Name}</h1>
                         <p className='text-white text-2xl'>{infos.description}</p>
                     </div>
-                    <hr className='border-white' />
+                    <hr className='border-white border-x-8 ' />
                     <div className='flex flex-row items-center justify-center gap-x-8'>
+                        <div className='flex flex-col items-center justify-center'>
                         <span className='text-white text-3xl'>R${infos.price}</span>
+                        <span className='text-white text-2xl'>Em até 6x sem juros!</span>
+                        </div>
                         <div className='flex flex-col items-center justify-center gap-y-3'>
                             <Button
                             text='Adicionar no carrinho'
