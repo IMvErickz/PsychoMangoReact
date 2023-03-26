81fcfad091a374169d2f18362ab6312a31dabade
IMvErickz <erickspy2003@gmail.com>
2023-03-26 17:30:54 -0300

    added react-loading

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -4,7 +4,7 @@ import { api } from "../../lib/axios";
 import { Link } from "react-router-dom";
 
 import '../assets/ImgProducts/fireforce.png'
-import axios from "axios";
+import { Loading } from "../components/Loading";
 import { useQuery } from "react-query";
 import { PsychoLogo } from "../assets/incons/logo";
 import { Input } from "../components/input";
@@ -46,7 +46,7 @@ export function Home() {
             </nav> <br />
             
             <div className="w-full h-full items-center justify-center grid sm:grid-rows-1 md:grid-rows-2 grid-rows-4 grid-flow-col gap-4">
-                {data?.map(products => {
+                {data ? data?.map(products => {
                     return (
                         <Link to='/product'>
                         <FieldProducts
@@ -57,7 +57,7 @@ export function Home() {
                             />
                         </Link>
                     )
-                })}
+                }) : <Loading/>}
             </div>
         </div>
     )
--- src/screens/productInfo.tsx
+++ src/screens/productInfo.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import { api } from '../../lib/axios'
-import ChawSaw from '../assets/ImgProducts/Chawsaw.png'
 import { Button } from '../components/button'
 
 interface InfoProps{
