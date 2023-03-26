7abcb7d1b4b54dfecdd2d1e1cd69abab6e60432c
IMvErickz <erickspy2003@gmail.com>
2023-03-26 17:47:10 -0300

    fixed: loading color

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 
 export const Loading = () => (
-	<ReactLoading type={'bars'} color={"yellow"} height={367} width={375} />
+	<ReactLoading type={'bars'} color={"#FFD222"} height={367} width={375} />
 );
